cd3d4990d45bbb0ca6a0ee6961f9151287b80c9d
IT18044096 <it18044096@my.sliit.lk>
2021-11-05 16:10:28 +0530

    Implement Form I-6

---
+++ InternshipManagementSystem/Client/src/Forms/FormI6/FormI6.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class FormI6 extends Component {
+    render() {
+        return (
+            <div id="main-FormI6-container">
+                <div id="ia">
+                    <h3 className="text-secondary text-center">Form I – 6</h3>
+                    <h5 className="text-secondary text-center">Internship Report</h5><br/>
+                    <a>Click here to download Form I-6 Template</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FormI6;
