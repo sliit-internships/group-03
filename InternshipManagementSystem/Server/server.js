10d9ac0002eb8b90a8b6d0a378fa10b77fc47744
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2021-12-14 20:59:20 +0530

    all files added

--- InternshipManagementSystem/Server/server.js
+++ InternshipManagementSystem/Server/server.js
@@ -1,35 +1,38 @@
-require('dotenv').config({path: "./config.env"});
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+app.use(cookieParser());
+app.use(express.json());
 const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
+require('dotenv').config({path: "./config.env"});
 
-//Connect DB
+const Port = process.env.PORT;
+
 connectDB();
 
-const app = express();
 //routes
 const formI1ARoutes=require("./routes/FormI1ARoutes");
 const formI1CRoutes=require("./routes/FormI1CRoutes");
 const formI1BRoutes=require("./routes/FormI1BRoutes");
 const formI5ARoutes=require("./routes/FormI5ARoutes");
 const formI3AOnlineRoutes=require("./routes/FormI1BOnlineRoutes");
-//middleware
-app.use(express.json());
+const userRouter = require('./routes/User');
 
 // Configuring middlewares
 app.use(express.static('public')); //to access the files in public folder
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(cors());
 app.use(fileUpload());
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/private', require('./routes/private'));
+
+app.use('/user',userRouter);
 
 // Configuring routes
 app.use("/api/formI1A", formI1ARoutes.router);
@@ -41,33 +44,25 @@ app.use("/api/formI1BOnline", formI3AOnlineRoutes.router);
 // file upload api
 app.post('/api/upload', (req, res) => {
 
-  if (!req.files) {
-    return res.status(500).send({ msg: "file is not found" })
-  }
-  // accessing the file
-  const myFile = req.files.file;
-
-  //  places the file inside public directory
-  myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
-    if (err) {
-      console.log(err)
-      return res.status(500).send({ msg: "Error occured" });
+    if (!req.files) {
+        return res.status(500).send({ msg: "file is not found" })
     }
-    // returing the response with file path and name
-    return res.send({name: myFile.name, path: `/${myFile.name}`});
-  });
+    // accessing the file
+    const myFile = req.files.file;
+
+    //  places the file inside public directory
+    myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send({ msg: "Error occured" });
+        }
+        // returing the response with file path and name
+        return res.send({name: myFile.name, path: `/${myFile.name}`});
+    });
 })
 
-//Error Handler
-app.use(errorHandler);
+app.listen(Port, () => {
 
-const PORT = process.env.PORT || 8181;
+    console.log('App is running on port', Port)
 
-const server = app.listen(PORT, () => 
-  console.log(`Server is running on ${PORT}`)
-);
-
-process.on("unhandledRejection", (err, promise) =>{
-  console.log(`Logged Error: ${err.message}`);
-  server.close(() => process.exit(1));
-})
+});
