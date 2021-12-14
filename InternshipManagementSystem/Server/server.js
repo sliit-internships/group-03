75cc043be80fec31ceb2ce2aad829af6ac1a2886
IT18044096 <malavigeoshani@gmail.com>
2021-12-14 14:20:56 +0530

    implement file upload api

--- InternshipManagementSystem/Server/server.js
+++ InternshipManagementSystem/Server/server.js
@@ -38,6 +38,26 @@ app.use("/api/formI1B", formI1BRoutes.router);
 app.use("/api/formI5A", formI5ARoutes.router);
 app.use("/api/formI1BOnline", formI3AOnlineRoutes.router);
 
+// file upload api
+app.post('/api/upload', (req, res) => {
+
+  if (!req.files) {
+    return res.status(500).send({ msg: "file is not found" })
+  }
+  // accessing the file
+  const myFile = req.files.file;
+
+  //  places the file inside public directory
+  myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+    if (err) {
+      console.log(err)
+      return res.status(500).send({ msg: "Error occured" });
+    }
+    // returing the response with file path and name
+    return res.send({name: myFile.name, path: `/${myFile.name}`});
+  });
+})
+
 //Error Handler
 app.use(errorHandler);
 
